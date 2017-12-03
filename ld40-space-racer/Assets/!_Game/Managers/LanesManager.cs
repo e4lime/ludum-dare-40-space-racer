84ef6fd4c5c111f840d7a3900aa69607e9f734c6
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-03 14:56:20 +0100

    Add basic lanemanager

---
+++ ld40-space-racer/Assets/!_Game/Managers/LanesManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace E4lime.LudumDare.Ld40.Lanes {
+	public class LanesManager : MonoBehaviour {
+
+		[SerializeField]
+		private Transform[] m_Lanes;
+
+		private int m_CurrentLaneIndex;
+
+		void Awake() {
+			m_CurrentLaneIndex = 1;
+		}
+
+		public Transform GetLeftLane() {
+			--m_CurrentLaneIndex;
+			if (m_CurrentLaneIndex < 0) {
+				m_CurrentLaneIndex = 0;
+				return null;
+			}
+
+			return m_Lanes[m_CurrentLaneIndex];
+		}
+
+		public Transform GetRightLane() {
+			
+			++m_CurrentLaneIndex;
+			if (m_CurrentLaneIndex >= m_Lanes.Length) {
+				m_CurrentLaneIndex = m_Lanes.Length-1;
+				return null;
+			}
+
+			return m_Lanes[m_CurrentLaneIndex];
+		}
+
+	}
+}
