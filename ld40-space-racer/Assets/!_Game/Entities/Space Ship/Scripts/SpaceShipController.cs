23992db5dfc804941bf0f511fbbebb3786a36ea7
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-03 14:56:31 +0100

    Add basic shipcontroller

---
+++ ld40-space-racer/Assets/!_Game/Entities/Space Ship/Scripts/SpaceShipController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace E4lime.LudumDare.Ld40.SpaceShip {
+	public class SpaceShipController : MonoBehaviour {
+		
+	
+
+		private SpaceShipBehaviour m_SpaceShipBehaviour;
+
+		void Awake(){
+			m_SpaceShipBehaviour = GetComponent<SpaceShipBehaviour>();
+		}
+
+		void Start() { 
+		}
+
+		private void Update() {
+		
+			if (Input.GetButton("MoveLeft")) {
+				m_SpaceShipBehaviour.MoveLeft();
+			}
+
+			 if (Input.GetButton("MoveRight")) {
+				m_SpaceShipBehaviour.MoveRight();
+			}
+		}
+
+
+	}
+}
