1c7910e3a5f3dc1fb87a09259c59132ee6184ed6
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-04 01:31:28 +0100

    Gun that spawns obstacles, probably wont be used

---
+++ ld40-space-racer/Assets/!_Game/Script Components/ObstacleGun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace E4lime.LudumDare.Ld40.SpaceShip {
+	public class ObstacleGun : MonoBehaviour {
+		
+		[SerializeField]
+		private Transform m_ObstacleToLaunch;
+
+		[SerializeField]
+		private Transform m_InstantiatedObjects;
+
+		private Rigidbody m_Rigidbody;
+		private Transform m_Transform;
+
+		void Awake(){
+			m_Rigidbody = GetComponent<Rigidbody>();
+			m_Transform = transform;
+		}
+
+		void Start() { 
+		}
+
+		private void Update() {
+			if (Input.GetButtonDown("FireLauncher")) {
+				Instantiate(m_ObstacleToLaunch,this.m_Transform.position + Vector3.forward * 130,Quaternion.identity,m_InstantiatedObjects);
+			}
+		}
+	}
+}
