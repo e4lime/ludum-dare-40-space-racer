0e918911163918802b1a3e5fd680596723900c9b
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-04 01:30:14 +0100

    Add obstacle

---
+++ ld40-space-racer/Assets/!_Game/Entities/Obstacle/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace E4lime.LudumDare.Ld40.Level {
+	public class Obstacle : MonoBehaviour {
+
+		private Rigidbody m_Rigidbody;
+		private Transform m_Transform;
+
+		void Awake(){
+			m_Rigidbody = GetComponent<Rigidbody>();
+			m_Transform = transform;
+		}
+
+		void Start() { 
+		}
+
+		void OnTriggerEnter(Collider other) {
+			if (other.CompareTag("Player")) { 
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
