eb182ed291a13c005229ef16eee19ca0863fa9cd
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-04 14:00:51 +0100

    Add health

---
+++ ld40-space-racer/Assets/!_Game/Script Components/Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace E4lime.LudumDare.Ld40.Components {
+	public class Health : MonoBehaviour {
+		
+		[SerializeField]
+		private float m_Health = 100f;
+
+		public float HealthValue {
+			get { return m_Health; }
+			set { m_Health = value; }
+		}
+
+		public float DecreaseHealth(int value) {
+			m_Health -= value;
+			return m_Health;
+		}
+	}
+}
