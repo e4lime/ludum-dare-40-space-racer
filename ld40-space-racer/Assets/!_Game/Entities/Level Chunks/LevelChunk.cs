ca58c78ef9bab292fddd734960b7873f1f523d82
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-04 17:25:01 +0100

    Remove unused code

--- ld40-space-racer/Assets/!_Game/Entities/Level Chunks/LevelChunk.cs
+++ ld40-space-racer/Assets/!_Game/Entities/Level Chunks/LevelChunk.cs
@@ -9,14 +9,6 @@ namespace E4lime.LudumDare.Ld40.InfiniteLevel {
 		[SerializeField]
 		private float m_ZLengthOfChunk = 10f;
 
-		private Rigidbody m_Rigidbody;
-		private Transform m_Transform;
-
-		void Awake(){
-			m_Rigidbody = GetComponent<Rigidbody>();
-			m_Transform = transform;
-		}
-
 
 
 		public float GetZLength() {
