bd371a39dc81ed8e636da25cb474b00d267b19ad
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-04 14:01:01 +0100

    Basic gameplay

---
+++ ld40-space-racer/Assets/!_Game/Managers/GameplayManager.cs
@@ -0,0 +1,32 @@
+﻿using E4lime.LudumDare.Ld40.SpaceShip;
+using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+using E4lime.LudumDare.Ld40.Components;
+namespace E4lime.LudumDare.Ld40 {
+	public class GameplayManager : MonoBehaviour {
+		
+	
+
+		private SpaceShipBehaviour m_SpaceShipBehaviour;
+		private Health m_PlayerHealth;
+
+		void Awake(){
+			m_SpaceShipBehaviour = FindObjectOfType<SpaceShipBehaviour>();
+			m_PlayerHealth = m_SpaceShipBehaviour.GetComponent<Health>();
+		}
+
+		void Update() { 
+			if (m_PlayerHealth.HealthValue <= 0) {
+				GameOver();
+			}
+		}
+
+		private void GameOver() {
+			Destroy(this.m_SpaceShipBehaviour.gameObject);
+		}
+	}
+
+	
+}
