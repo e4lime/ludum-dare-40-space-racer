3fe982314632693516dd3fbb444d1628ca53ce82
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-05 04:11:04 +0100

    Add instructions to online highscore

--- Templates/HighScoreCodes_EXAMPLE.cs
+++ Templates/HighScoreCodes_EXAMPLE.cs
@@ -1,5 +1,7 @@
 namespace E4lime.LudumDare.Ld40 {
 	/**
+	 * Go to: http://dreamlo.com and click "Get yours now"
+         * Copy and paste the private and public key code to this file
 	 * Dont commit to git
 	 **/
 	public class HighscoreCodes {
