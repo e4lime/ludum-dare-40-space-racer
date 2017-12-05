d0d46ca30d85d6b2febf988dea1214017ceb0cd7
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-05 02:34:18 +0100

    Wont be used

---
+++ ld40-space-racer/Assets/!_Game/Script Components/RandomWidthOnAwake.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace E4lime.LudumDare.Ld40.Components {
+	public class RandomWidthOnAwake : MonoBehaviour {
+		
+	
+		private float m_WidthRangeMax = 3f;
+
+		void Start(){
+			float newX = Random.Range(transform.localScale.x, transform.localScale.x + m_WidthRangeMax);
+			transform.localScale = new Vector3(newX, transform.localScale.y, transform.localScale.z );
+			Debug.Log(newX);	
+		}
+
+	}
+}
