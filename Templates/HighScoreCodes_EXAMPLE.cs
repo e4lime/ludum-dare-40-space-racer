8599a76326dfda9ec02302a382cadcd611a93969
Emil Lindberg <emil.lindberg86@gmail.com>
2017-12-05 04:00:43 +0100

    add example highscore code file for readme

---
+++ Templates/HighScoreCodes_EXAMPLE.cs
@@ -0,0 +1,11 @@
+namespace E4lime.LudumDare.Ld40 {
+	/**
+	 * Dont commit to git
+	 **/
+	public class HighscoreCodes {
+		public const string privateCode = ""; 
+		public const string publicCode = "";
+		public const string webURL = "http://dreamlo.com/lb/";
+	}
+	
+}
