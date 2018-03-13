76037a304792d38d2f3ed3f6b9588ff5c1baf9f1
Emil Lindberg <emil.lindberg86@gmail.com>
2018-03-13 11:36:04 +0100

    Add PlayerSettings for android

---
+++ ld40-space-racer/Assets/!_Game/PlayerSettings.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace E4lime.LudumDare.Ld40 {
+	public class PlayerSettings : MonoBehaviour {
+
+
+#if UNITY_ANDROID
+		void Awake() {
+
+
+			Application.targetFrameRate = 60;
+
+			QualitySettings.vSyncCount = 0;
+
+			QualitySettings.antiAliasing = 0;
+
+
+	
+			QualitySettings.shadowCascades = 0;
+			QualitySettings.shadowDistance = 15;
+			
+
+
+			QualitySettings.shadowCascades = 2;
+			QualitySettings.shadowDistance = 70;
+			
+
+			Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+
+		}
+
+#endif
+	}
+}
