1681b43a05c217003bc1a8713ca12f26d1bf7beb
Emil Lindberg <emil.lindberg86@gmail.com>
2018-03-13 12:19:14 +0100

    Add android input and wrap input with preprocessors

--- ld40-space-racer/Assets/!_Game/Entities/Space Ship/Scripts/SpaceShipController.cs
+++ ld40-space-racer/Assets/!_Game/Entities/Space Ship/Scripts/SpaceShipController.cs
@@ -17,16 +17,29 @@ namespace E4lime.LudumDare.Ld40.SpaceShip {
 		}
 
 		private void Update() {
-		
+#if UNITY_STANDALONE || UNITY_EDITOR
 			if (Input.GetButtonDown("MoveLeft")) {
 				m_SpaceShipBehaviour.MoveLeft();
 			}
 
-			 if (Input.GetButtonDown("MoveRight")) {
+			if (Input.GetButtonDown("MoveRight")) {
 				m_SpaceShipBehaviour.MoveRight();
 			}
+
+#endif
+
+#if UNITY_ANDROID
+			foreach (Touch touch in Input.touches) {
+				if (touch.phase != TouchPhase.Began)
+					return;
+				if (touch.position.x < Screen.width / 2) {
+					m_SpaceShipBehaviour.MoveLeft();
+				}
+				else {
+					m_SpaceShipBehaviour.MoveRight();
+				}
+			}
+#endif
 		}
-
-
 	}
 }
